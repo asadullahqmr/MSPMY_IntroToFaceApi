9e52243dfaf2d50d8440e98ae7bdc09eeabdbf90
asadullahqmr <asadqb16@gmail.com>
2020-05-16 05:51:18 +0800

    Fixed a mistake.

--- MSPMY_IntroToFaceApi/MSPMY_IntroToFaceApi/SecureStorageService.cs
+++ MSPMY_IntroToFaceApi/MSPMY_IntroToFaceApi/SecureStorageService.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Essentials;
 
-namespace FacialRecognitionLogin
+namespace MSPMY_IntroToFaceApi
 {
     public static class SecureStorageService
     {
